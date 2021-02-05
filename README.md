24e74ff522c182aa0ef9a377961f7ce55ce70d19
MarkCode7n <mkmark53@gmail.com>
2021-02-05 06:52:17 +0300

    commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# music-website
+# Music portfolio  website
+### Features :
+* play music
+* music gallery - lightBox
+* send messages through netlify
+* scroll reveal
+* album page
+* blog page
+## ....and many more .....
--- albums.html/albums.js
+++ albums.html/albums.js
@@ -8,15 +8,24 @@ window.addEventListener("load", function () {
 
 window.DefineWeb.Playlist = function () {
     !(() => {
-        let audio, path, ext, IsPlaying, PlaylistTracks, PlayerElem, PLayingTrack;
+        let audio, path, ext, IsPlaying, PlaylistTracks1, PlaylistTracks2, PlaylistTracks3, PLayingTrack;
         audio = new Audio;
         audio.volume = 0.13;
         path = "./audios/";
         ext = ".mp3";
-        PlayerElem = document.getElementById("player");
+        // checking for fallbacks for our extension ".mp3"
+        /** 
+         * @ext = mp3 || others
+         */
+        let agent = navigator.userAgent.toLocaleLowerCase()
+        if (agent.indexOf("firefox") !== -1 || agent.indexOf("opera") != -1) {
+            ext = ".ogg"
+        }
         IsPlaying = false;
-        // PlaylistTracks : [name,src]->
-        PlaylistTracks = {
+
+
+        // PlaylistTracks1 : [name,src]->
+        PlaylistTracks1 = {
             "t1": ["Baby don't cry", "BABY_DONT_CRY000"],
             "t2": ["Not Afraid", "Eminem"],
             "t3": ["Lumba", "LUMBA"],
@@ -24,36 +33,26 @@ window.DefineWeb.Playlist = function () {
             "t5": ["Nutorious", "Nutorious"],
             "t6": ["Ssasa", "Ssasa"],
         }
-        for (const key in PlaylistTracks) {
-            //create
-            const player__item = document.createElement("li");
-            const Player__name = document.createElement("div");
-            const play__btn = document.createElement("button");
-
-            //add class
-            player__item.classList.add("player__item");
-            Player__name.classList.add("player__item-name");
-            play__btn.classList.add("player__item-button");
-            // play__btn.classList.add("play");
-
-            //set id for the btn in order to play an audio with the id as the src
-            play__btn.id = PlaylistTracks[key][1];
-
-            //playing audio and switching tracks
-            play__btn.addEventListener("click", (e) => {
-                switchTrack(e);
-            })
-            //merge ans set created elements
-            Player__name.textContent = PlaylistTracks[key][0]
-            player__item.appendChild(Player__name);
-            player__item.appendChild(play__btn);
-            PlayerElem.appendChild(player__item);
+        //..
+        PlaylistTracks2 = {
+            "t1": ["Baby don't cry", "BABY_DONT_CRY000"],
+            "t2": ["Not Afraid", "Eminem"],
+            "t3": ["Lumba", "LUMBA"],
+            "t4": ["Mysterious girl", "Mysterious_Girl"],
+            "t5": ["Nutorious", "Nutorious"],
+            "t6": ["Ssasa", "Ssasa"],
         }
-        audio.addEventListener("ended", () => {
-            document.getElementById(PLayingTrack).classList.remove("pause");
-            PLayingTrack = "";
-            IsPlaying = false;
-        })
+        //..
+        PlaylistTracks3 = {
+            "t1": ["Baby don't cry", "BABY_DONT_CRY000"],
+            "t2": ["Not Afraid", "Eminem"],
+            "t3": ["Lumba", "LUMBA"],
+            "t4": ["Mysterious girl", "Mysterious_Girl"],
+            "t5": ["Nutorious", "Nutorious"],
+            "t6": ["Ssasa", "Ssasa"],
+        }
+
+        //..
         const switchTrack = (e) => {
             if (IsPlaying) {
                 if (PLayingTrack !== e.target.id) {
@@ -77,6 +76,46 @@ window.DefineWeb.Playlist = function () {
             }
             PLayingTrack = e.target.id;
         }
+        //..
+        const KeyInTracks = (PTrack, appendingElement) => {
+            for (const key in PTrack) {
+                //create
+                const player__item = document.createElement("li");
+                const Player__name = document.createElement("div");
+                const play__btn = document.createElement("button");
+
+                //add class
+                player__item.classList.add("player__item");
+                Player__name.classList.add("player__item-name");
+                play__btn.classList.add("player__item-button");
+                // play__btn.classList.add("play");
+
+                //set id for the btn in order to play an audio with the id as the src
+                play__btn.id = PTrack[key][1];
+
+                //playing audio and switching tracks
+                play__btn.addEventListener("click", (e) => {
+                    switchTrack(e);
+                })
+                //merge ans set created elements
+                Player__name.textContent = PTrack[key][0]
+                player__item.appendChild(Player__name);
+                player__item.appendChild(play__btn);
+                appendingElement.appendChild(player__item);
+            }
+        }
+
+        KeyInTracks(PlaylistTracks1, document.getElementById("player1"))
+
+        KeyInTracks(PlaylistTracks2, document.getElementById("player2"))
+
+        KeyInTracks(PlaylistTracks3, document.getElementById("player3"))
+
+        audio.addEventListener("ended", () => {
+            document.getElementById(PLayingTrack).classList.remove("pause");
+            PLayingTrack = "";
+            IsPlaying = false;
+        })
     })()
 }
 window.DefineWeb.SetNavigationBar = function () {
@@ -112,4 +151,3 @@ window.addEventListener("DOMContentLoaded", () => {
     let playlist = window.DefineWeb.Playlist();
     let SetNavigationBar = window.DefineWeb.SetNavigationBar();
 });
-
